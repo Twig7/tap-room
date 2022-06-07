77537fb29d805dd69291175f48507eaa061711ce
Amber W <amoothenielson@gmail.com>
2022-06-06 21:38:22 -0700

    Fiddle around with pre set list

---
+++ src/component/TapInfo.js
@@ -0,0 +1,30 @@
+import { v4 } from 'uuid';
+
+const TapInfo = [
+  {
+    name: "Ella's Juice",
+    brand: "Idk",
+    alcoholContent: 3,
+    price: 5,
+    quantity: 10,
+    id: v4()
+  },
+  {
+    name: "Twig's brew",
+    brand: "Idk",
+    alcoholContent: 5,
+    price: 10,
+    quantity: 20,
+    id: v4()
+  },
+  {
+    name: "Bandit's dream",
+    brand: "Idk",
+    alcoholContent: 10,
+    price: 50,
+    quantity: 5,
+    id: v4()
+  }
+]
+
+export default TapInfo;
